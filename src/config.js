352aff1f3e5d0a1a2924d4aaab256885dfe52e9f
Kamel <kamelo68@gmail.com>
2022-03-22 21:09:42 +0000

    Removed app id and token

--- src/config.js
+++ src/config.js
@@ -3,17 +3,17 @@ module.exports = function() {
         case 'development':
             return {
                 // Pass your app ID here.
-                appId: "15bd8f34ad054b3db9a2e133f8424e17",
+                appId: "YOUR_APP_ID",
                 // Set the channel name.
                 channel: "demo_channel_name", // "demo_channel_name"
                 // Pass a token if your project enables the App Certificate.
-                token: "NETLESSSDK_YWs9VkctX2tNRTM0VjdPNjl4bSZub25jZT05YzVlMzBkMC1hODYzLTExZWMtOGFkZS05YjA2MGI3MWYyNDkmcm9sZT0wJnNpZz1mOGYxYWViN2JjMDU2NTMyYjU5YTIyNmMyOGUxZTE5M2U5ZWQ4N2ZmNTM4ZjllYWE0OTg0MDlmNmUzYjA0MWVm",
+                token: "TOKEN_HERE",
                 uid: null,
             };
 
         case 'production':
             return { // Pass your app ID here.
-                appId: "15bd8f34ad054b3db9a2e133f8424e17",
+                appId: "YOUR_APP_ID",
                 // Set the channel name.
                 channel: null,
                 // Pass a token if your project enables the App Certificate.
